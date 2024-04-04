2f7b38a1cdd3b3db623d75778972f6cd84cb408c
robin hunter <rbnrggs@yahoo.com>
2024-04-04 10:14:02 -1000

    Update main page

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>WELCOME to NEXT NEON VERCEL</h1>
+      <h1>WELCOME to NEXT | NEON | VERCEL</h1>
+      <h3>We are building the future...</h3>
     </main>
   );
 }
